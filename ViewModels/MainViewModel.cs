a538e3a04264c5c815698018b33d616463d5dc97
Ngoc20041 <buin1905@gmail.com>
2025-06-13 18:01:37 +0700

    Update

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,18 +1,17 @@
-﻿using Avalonia.Svg.Skia;
+﻿using System;
+using Avalonia.Svg.Skia;
 using CommunityToolkit.Mvvm.ComponentModel;
-using Avalonia.Media.Imaging;
 namespace LearnAvalonia.ViewModels;
-
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 public partial class MainViewModel : ViewModelsBase
 {
     [ObservableProperty]
     [NotifyPropertyChangedFor(nameof(SideMenuImage))]
-    private bool _SideMenuExpaned = true;
+    private bool _sideMenuExpaned = true;   
     // public SvgImage SideMenuImage => new SvgImage{Source = SvgSource.Load($"avares://{nameof(LearnAvalonia)}/Assets/Images/Icons/{(SideMenuExpaned ? "logo-icon":"logo")}.svg")};
     // public Bitmap SideMenuImage => new Bitmap($"avares://{nameof(LearnAvalonia)}/Assets/Images/Icons/{(SideMenuExpaned ? "logo" : "logo-icon")}.png");
-    
-    
-    
-    
-    public Bitmap SideMenuImage => new Bitmap("Assets/Images/Icons/logo-icon.png");
+    public Bitmap SideMenuImage => new Bitmap(
+        AssetLoader.Open(new Uri($"avares://{nameof(LearnAvalonia)}/Assets/Images/Icons/{(SideMenuExpaned ? "logo-icon" : "logo")}.png"))
+    );
 }
